5df9d6a02c5a6e1ccb4c11b194c0cfd7a8c037e2
Gerardo <gerardo.riveran@softtek.com>
2021-06-09 12:52:08 +0200

    commit iva.js

--- iva.js
+++ iva.js
@@ -1,3 +1,10 @@
-function iva() {
-    console.log("iva");
+function calcularIva(importe){
+    return importe * 21 /100
 }
+function calcularPago(importe,iva){
+    return importe +iva
+}
+let importe = 800;
+let modelo = "Custom";
+let iva = calcularIva(importe);
+console.log("Tabla Burton: "+ modelo + " -> A pagar "+calcularPago(importe,iva));
